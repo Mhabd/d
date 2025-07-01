5bce351224ce8c4df034f0443ee067bb14b67a32
da <108029568+Mhabd@users.noreply.github.com>
2025-07-01 10:27:15 -0700

    Update j.bat

--- j.bat
+++ j.bat
@@ -1,9 +1,10 @@
-try {
-  var xhr = new ActiveXObject("Microsoft.XMLHTTP");
-  xhr.open("POST", "https://discordapp.com/api/webhooks/1366207428458315916/JmzBOIOIc-BVCutPDn4WirzTJQzbltkDiXz1UZZSFy9sWdDvufh6iRmJ7cjsLzgHex7W", false);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send('{ "content": "<@everyone> Ping from JScript in PowerShell!" }');
-  WScript.Echo("Status: " + xhr.status);
-} catch(e) {
-  WScript.Echo("Error: " + e.message);
-}
+$js = @"
+var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+xhr.open("POST", "https://discordapp.com/api/webhooks/1366207428458315916/JmzBOIOIc-BVCutPDn4WirzTJQzbltkDiXz1UZZSFy9sWdDvufh6iRmJ7cjsLzgHex7W", false);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.send('{ "content": "<@everyone> Ping from JScript in PowerShell!" }');
+"@
+
+$jsPath = "$env:TEMP\webhook.js"
+$js | Out-File -Encoding ASCII $jsPath
+Start-Process "cscript.exe" -ArgumentList "//Nologo", $jsPath
